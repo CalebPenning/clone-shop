a47ffea5b2fb4503c6da22fd3d3c96ced4dd534f
calebpenning <calebpenning@mail.com>
2021-04-18 22:45:19 -0700

    modified production settings for heroku

--- app.py
+++ app.py
@@ -14,7 +14,7 @@ app = Flask(__name__)
 app.config['SQLALCHEMY_DATABASE_URI'] = (os.environ.get(
     'DATABASE_URL',
     DATABASE_URI
-))
+).replace("postgres://", "postgresql:///"))
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 app.config['SQLALCHEMY_ECHO'] = True
 app.config['DEBUG_TB_INTERCEPT_REDIRECTS'] = False
